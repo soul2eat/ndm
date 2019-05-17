e4dff81df8b38070b679d667c52dc78ff9607a3c
soul2eat <45907500+soul2eat@users.noreply.github.com>
2019-05-17 09:23:48 +0300

    Test update

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 
-require('update-electron-app')({
-  repo: 'soul2eat/ndm',
-  updateInterval: '5 minutes',
+const {app, BrowserWindow, shell, dialog, Tray, Menu, ipcMain} = require('electron');
+const { autoUpdater } = require("electron-updater");
 
-})
-const {app, BrowserWindow, shell, dialog, Tray, Menu} = require('electron')
-if (handleSquirrelEvent(app)) {
-// squirrel event handled and app will exit in 1000ms, so don't do anything else
-return;
-}
+
 
 let mainWindow;
 let tray;
@@ -39,6 +33,7 @@ app.on('ready', ()=>{
   ]);
   tray.setToolTip('Node Download Manager');
   tray.setContextMenu(contextMenu);
+  autoUpdater.checkForUpdatesAndNotify();
   })
 
 // Quit when all windows are closed.
@@ -53,6 +48,32 @@ app.on('activate', function () {
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
 })
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
 
 var WebSocket = require('ws');
@@ -497,54 +518,3 @@ function mkdir(dirpath) {
     if (err.code !== 'EEXIST') return fase;
   }
 }
-
-
-function handleSquirrelEvent(application) {
-if (process.argv.length === 1) {
-return false;
-}
-const ChildProcess = require('child_process');
-const path = require('path');
-const appFolder = path.resolve(process.execPath, '..');
-const rootAtomFolder = path.resolve(appFolder, '..');
-const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-const exeName = path.basename(process.execPath);
-const spawn = function(command, args) {
-let spawnedProcess, error;
-try {
-spawnedProcess = ChildProcess.spawn(command, args, {
-detached: true
-});
-} catch (error) {}
-return spawnedProcess;
-};
-const spawnUpdate = function(args) {
-return spawn(updateDotExe, args);
-};
-const squirrelEvent = process.argv[1];
-switch (squirrelEvent) {
-case '--squirrel-install':
-case '--squirrel-updated':
-// Optionally do things such as:
-// - Add your .exe to the PATH
-// - Write to the registry for things like file associations and
-//   explorer context menus
-// Install desktop and start menu shortcuts
-spawnUpdate(['--createShortcut', exeName]);
-setTimeout(application.quit, 1000);
-return true;
-case '--squirrel-uninstall':
-// Undo anything you did in the --squirrel-install and
-// --squirrel-updated handlers
-// Remove desktop and start menu shortcuts
-spawnUpdate(['--removeShortcut', exeName]);
-setTimeout(application.quit, 1000);
-return true;
-case '--squirrel-obsolete':
-// This is called on the outgoing version of your app before
-// we update to the new version - it's the opposite of
-// --squirrel-updated
-application.quit();
-return true;
-}
-};
